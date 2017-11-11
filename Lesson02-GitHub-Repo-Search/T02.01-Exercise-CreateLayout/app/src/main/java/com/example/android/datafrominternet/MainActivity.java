956dfc362fdb8c7b0e249d8d3bbbf0427743844e
EricAndreR <ericandre89@gmail.com>
2017-11-11 17:36:45 +0000

    Completed TODOs 26 to 32.

--- Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,24 +15,34 @@
  */
 package com.example.android.datafrominternet;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
+    // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
+    // COMPLETED (27) Create a TextView variable called mUrlDisplayTextView
+    // COMPLETED (28) Create a TextView variable called mSearchResultsTextView
 
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    EditText mSearchBoxEditText;
+    EditText mUrlDisplayTextView;
+    EditText mSearchResultsTextView;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+        // COMPLETED (29) Use findViewById to get a reference to mSearchBoxEditText
+        // COMPLETED (30) Use findViewById to get a reference to mUrlDisplayTextView
+        // COMPLETED (31) Use findViewById to get a reference to mSearchResultsTextView
 
-        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = (EditText) findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = (EditText) findViewById(R.id.tv_github_search_results_json);
+
     }
 }
