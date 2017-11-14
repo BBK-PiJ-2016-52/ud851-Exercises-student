59485043ab7a3b5656690c2030c4d0d541ce481a
EricAndreR <ericandre89@gmail.com>
2017-11-14 18:00:17 +0000

    Completed variable creation and references.

--- Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
@@ -36,9 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
-    // TODO (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessageTextView;
 
-    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mProgressBar;
+
+    // COMPLETED (12) Create a variable to store a reference to the error message TextView
+
+    // COMPLETED (24) Create a ProgressBar variable to store a reference to the ProgressBar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +54,13 @@ public class MainActivity extends AppCompatActivity {
 
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        mErrorMessageTextView = (TextView) findViewById(R.id.tv_error_message_display);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
-        // TODO (13) Get a reference to the error TextView using findViewById
 
-        // TODO (25) Get a reference to the ProgressBar using findViewById
+        // COMPLETED (13) Get a reference to the error TextView using findViewById
+
+        // COMPLETED (25) Get a reference to the ProgressBar using findViewById
     }
 
     /**
@@ -69,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+
 
     // TODO (15) Create a method called showErrorMessage to show the error and hide the data
 
